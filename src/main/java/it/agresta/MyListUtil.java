79bac490d04b6beca70659104620a1229e08854d
Giuseppe <peri.cle991@gmail.com>
2020-11-04 11:50:18 +0100

    Implemented MyListUtil and test class

--- src/main/java/it/agresta/MyListUtil.java
+++ src/main/java/it/agresta/MyListUtil.java
@@ -1,4 +1,26 @@
 package it.agresta;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class MyListUtil {
+    public List<Integer> getSortedList(List<Integer> l, boolean choice) {
+        if(choice)
+            l.sort(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return (o1 - o2);
+                }
+            });
+        else
+            l.sort(new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return (o2 - o1);
+                }
+            });
+
+
+        return l;
+    }
 }
--- src/test/java/it/agresta/MyListUtilTest.java
+++ src/test/java/it/agresta/MyListUtilTest.java
@@ -1,4 +1,27 @@
 package it.agresta;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
 public class MyListUtilTest {
+    public static MyListUtil obj;
+
+    @BeforeClass
+    public static void initialize() {
+        obj = new MyListUtil();
+    }
+
+    @Test
+    public void sortingTest() {
+        assertEquals(Arrays.asList(), obj.getSortedList(Arrays.asList(), true));
+        assertEquals(Arrays.asList(), obj.getSortedList(Arrays.asList(), false));
+        assertEquals(Arrays.asList(1, 2, 3, 4), obj.getSortedList(Arrays.asList(3, 2, 1, 4), true));
+        assertEquals(Arrays.asList(4, 3, 2, 1), obj.getSortedList(Arrays.asList(3, 2, 1, 4), false));
+        assertEquals(Arrays.asList(1, 1, 2, 2, 3), obj.getSortedList(Arrays.asList(3, 2, 1, 2, 1), true));
+        assertEquals(Arrays.asList(3, 2, 2, 1, 1), obj.getSortedList(Arrays.asList(3, 2, 1, 2, 1), false));
+    }
 }
